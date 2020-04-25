1ec90e8c3d0f22f105f475a0f1518e9342de150c
Ruslan2322 <62638170+Ruslan2322@users.noreply.github.com>
2020-04-25 10:50:53 +0300

    Ex_19

---
+++ Ex_19.java
@@ -0,0 +1,32 @@
+package Lesson1;
+
+
+// Даны два числа. Определить цифры, входящие в запись как первого так и второго числа.
+
+     public class Ex_19 {
+        public static void main(String[] args) {
+
+            int[] number1 = {1, 3, 4};
+            int[] number2 = {2, 3, 4};
+            for (int v : number1)
+                System.out.print(v);
+            System.out.print(" ");
+            for (int v1 : number2)
+                System.out.print(v1);
+            {
+                int[] number3 = new int[number1.length];
+                int count = 0;
+
+                for (int i = 0; i < number1.length;i++) {
+                    if (number1[i] == number2[i]) {
+                        number3[count] = number1[i];
+                        count++;
+                        System.out.println(" ");
+                        for (int v2 : number3)
+                            System.out.print(v2);
+
+                    }
+                }
+            }
+        }
+    }
