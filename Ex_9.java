952972b9bc0cda1c988ab65bbc13e04712d41241
Ruslan2322 <62638170+Ruslan2322@users.noreply.github.com>
2020-04-25 10:14:36 +0300

    Ex_9

---
+++ Ex_9.java
@@ -0,0 +1,18 @@
+package Lesson1;
+
+
+//Даны три точки A(x1,y1),B(x2,y2) и C(x3,y3). Определить , будут ли они расположены на одной прямой.
+
+public class Ex_9 {
+    public static void main(String[] args) {
+
+        double x1 = 1, y1 = 1;
+        double x2 = 1, y2 = 1;
+        double x3 = 1, y3 = 1;
+
+        if (x1 == x2 && x2 == x3 && x1 == x3 && y1 == y2 && y2 == y3 && y1 == y3)
+            System.out.println("Точки на одной прямой");
+        else
+            System.out.println("Точки не лежат на одной прямой");
+    }
+}
