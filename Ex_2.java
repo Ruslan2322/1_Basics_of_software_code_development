c79dd7ac9ced309c3f8377b6dc0c808cc999e39b
Ruslan2322 <62638170+Ruslan2322@users.noreply.github.com>
2020-04-25 10:03:28 +0300

    Ex_2

---
+++ Ex_2.java
@@ -0,0 +1,24 @@
+package Lesson1;
+
+//Вычислить значение выражения по формуле (все переменные принимают действительные значения):
+//(b + sqrt(b^2 + 4*a*c))/2 - a^3*c +(1/b^2)
+
+public class Ex_2 {
+    public static void main(String... asf) {
+        // Объявим переменные для степеней переменных
+        int v1 = 2;
+        int v2 = 3;
+        int v3 = -2;
+        double z;
+// Объявим значения переменных с = 1.0 , b = 2.0, a = 3.0
+        double c = 5.0;
+        double a = 2.0, a1 = Math.pow(a, v2);
+        double b = 1.0, b1 = Math.pow(b, v1), b2 = Math.pow(b, v3);
+        double t = Math.sqrt(Math.pow(b, a1) + 4 * a * c);
+        double t1 = (b + t) % 2 * a;
+        z = t1 - a1 * c + b2;
+
+        System.out.println(z);
+
+    }
+}
