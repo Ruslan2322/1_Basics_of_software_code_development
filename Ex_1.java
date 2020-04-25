2a1b9afa3d0967818b3ae172ca12d8a582856834
Ruslan2322 <62638170+Ruslan2322@users.noreply.github.com>
2020-04-25 10:10:29 +0300

    Ex_1

---
+++ Ex_1.java
@@ -0,0 +1,18 @@
+package Lesson1;
+import java.util.Scanner;
+
+//Найдите  значение функции: z = ( (a – 3 ) * b / 2) + c.
+
+public class Ex_1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите значение а: ");
+        int a = s.nextInt();
+        System.out.println("Введите значение b: ");
+        int b = s.nextInt();
+        System.out.println("Введите значение c: ");
+        int c = s.nextInt();
+        double z = (((a-3)*b/2)+c);
+        System.out.println(z);
+    }
+}
