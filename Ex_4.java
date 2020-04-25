8339d35e095c7517ea854baf6056fa3cd8dda8fa
Ruslan2322 <62638170+Ruslan2322@users.noreply.github.com>
2020-04-25 10:06:40 +0300

    Ex_4

---
+++ Ex_4.java
@@ -0,0 +1,16 @@
+package Lesson1;
+
+// Дано действительное число R вида nnn.ddd(три цифровых разряда в дробной и целой частях).
+// Поменять местами дробную и целую части числа и вывести полученное значение числа.
+
+public class Ex_4 {
+    public static void main(String... asf) {
+// Предположим число R равно 457.987
+        double R = 457.987;
+        double R1 = (int) R;
+        //System.out.println(R1);
+        double result1 = R1 / 1000;
+        double result=  result1 + (R - R1) * 1000;
+        System.out.println(result);
+    }
+}
